96eb8d6d3bb842b2866b086a7c24b777d2c522a4
Ty Thanh Doan <tdoan35@gmail.com>
2021-04-01 04:09:51 -0700

    added chart to cash flow card

--- src/components/Overview/CashflowCard/CashflowCard.jsx
+++ src/components/Overview/CashflowCard/CashflowCard.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
-import { Button, ButtonGroup, Card, CardActions, CardContent, Typography, Divider, List } from '@material-ui/core';
+import React from 'react';
+import { Box, Button, ButtonGroup, Card, CardActions, CardContent, Typography, Divider, List } from '@material-ui/core';
+import { Doughnut } from '@reactchartjs/react-chart.js';
 
 import useStyles from './styles';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+}
 
 const CashflowCard = () => {
   
@@ -18,7 +46,16 @@ const CashflowCard = () => {
           <Divider />
           <List className={classes.list}>
             {/*--------------------- LIST ITEMS ---------------------*/}
-            
+            <Box alignItems="center" mt={3}>
+              <Doughnut 
+                data={data} 
+                options={{
+                  legend:{display: false},
+                  responsive: true,
+                  maintainAspectRatio: false,
+                }}
+              />
+            </Box>
             {/* ----------------------------------------------------- */}
           </List>
           <Divider />
